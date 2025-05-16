e1b6966e6416b6f9c7473f8937d2c313632358d4
bhagas koro <tump.store@gmail.com>
2025-05-16 15:24:17 +0700

    config

--- config.js
+++ config.js
@@ -4,14 +4,15 @@ import pg from 'pg';
 
 const { Pool } = pg
 const client = new Pool({
-     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB,
-    max: 20, 
-    idleTimeoutMillis: 30000,
-    connectionTimeoutMillis: 2000, 
-    ssl:true
+    //  host: process.env.DB_HOST,
+    // port: Number(process.env.DB_PORT),
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB,
+    // max: 20, 
+    // idleTimeoutMillis: 30000,
+    // connectionTimeoutMillis: 2000
+    connectionString: `postgres://postgres:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB}?sslmode=require`,
+  
   })
 export default client
